9cffce67d3deae9bc393f9d577d91963773091ca
Toshiaki Hatano <haeena@haeena.net>
2020-01-18 02:10:28 +0900

    add async example

--- example/example.py
+++ example/example.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 import uvicorn
@@ -8,17 +9,18 @@ from starlette.routing import Mount
 from slackevent_responder import SlackEventApp
 
 
-# Our app's Slack Event Adapter for receiving actions via the Events API
+# Setup SlackEventApp to receive Events from Slack API
 slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
 slack_events_app = SlackEventApp(
     path="/slack/events", slack_signing_secret=slack_signing_secret
 )
 
+# Mount SlackEventApp to Starlette
+app = Starlette(debug=True, routes=[Mount("/", slack_events_app)])
+
 # Create a SlackClient for your bot to use for Web API requests
 slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
 slack_client = SlackClient(slack_bot_token)
-
-app = Starlette(debug=True, routes=[Mount("/", slack_events_app)])
 
 
 # Example responder to greetings
@@ -28,17 +30,31 @@ def handle_message(event_data):
     # If the incoming message contains "hi", then respond with a "Hello" message
     if message.get("subtype") is None and "hi" in message.get("text"):
         channel = message["channel"]
-        message = "Hello <@%s>! :tada:" % message["user"]
+        message = f"Hello <@{message['user']}>! :tada:"
         slack_client.chat_postMessage(channel=channel, text=message)
 
 
 # Example reaction emoji echo
 @slack_events_app.on("reaction_added")
-def reaction_added(event_data):
+def say_reaction_added(event_data):
     event = event_data["event"]
     emoji = event["reaction"]
     channel = event["item"]["channel"]
-    text = ":%s:" % emoji
+    text = f"reaction_added :{emoji}:"
+    slack_client.chat_postMessage(channel=channel, text=text)
+
+
+# Example reaction async delayed emoji echo
+# multiple handler can subscribe a event
+@slack_events_app.on("reaction_added")
+async def say_twice_reaction_added(event_data):
+    event = event_data["event"]
+    emoji = event["reaction"]
+    channel = event["item"]["channel"]
+    text = (
+        "I will say twice because it's important\n" f"reaction_added :{emoji}:"
+    )
+    await asyncio.sleep(5)
     slack_client.chat_postMessage(channel=channel, text=text)
 
 
@@ -49,6 +65,6 @@ def error_handler(err):
 
 
 # Once we have our event listeners configured, we can start the
-# Flask server with the default `/events` endpoint on port 3000
+# uvicorn server with `/skack/events` endpoint on port 8000
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
